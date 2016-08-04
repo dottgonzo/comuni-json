d86b8b2c1a7ab1de527f2300a19b62a51cb1ece6
Dario Caruso <darioyzf@gmail.com>
2016-08-04 22:54:23 +0200

    up

---
+++ onlyworld.ts
@@ -0,0 +1,33 @@
+import Comuni from "./index"
+
+import * as _ from "lodash";
+
+
+import * as fs from "fs";
+
+
+const w=Comuni();
+
+
+_.map(w, function (continent) {
+
+    _.map(continent.subcontinents, function (subcontinent) {
+
+        _.map(subcontinent.countries, function (country) {
+
+country.states=[];
+
+        })
+
+    })
+
+})
+
+
+fs.writeFile("./worldwithoutstates.json", JSON.stringify(w), { encoding: "utf-8" }, function (err) {
+    if (err) {
+        console.log(err)
+    } else {
+    }
+
+})
